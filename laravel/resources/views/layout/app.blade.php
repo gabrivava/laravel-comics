f3f2354c5f720dc72b9c6a43b7fac402ff103c5c
gabrivava <gabriele.vavassori4@gmail.com>
2021-07-08 15:15:35 +0200

    aggiunto file fumetti in cartella config/ iniziato layout header/ collegato i vari file tra di loro

---
+++ laravel/resources/views/layout/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- font awesome --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>@yield('title')</title>
+    {{-- css --}}
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+
+    {{-- header --}}
+    <header id="site_header">
+
+        <div class="top_header">
+            <div>
+                <span>DC POWER℠ VISA®</span>
+                <span>ADDITIONAL DC SITE <i class="fas fa-sort-down"></i></span>
+            </div>
+        </div>
+        <div class="nav">
+            {{-- logo img --}}
+            <img src="" alt="">
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+
+    {{-- main --}}
+    <main>
+
+    </main>
+    
+    {{-- footer --}}
+    <footer id="site_footer">
+
+    </footer>
+</body>
+</html>
